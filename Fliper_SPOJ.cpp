c874204b4973d66b8dc9d9601835fc1b961d65ec
Luiz Eduardo de Almeida Santos <educelula@hotmail.com>
2017-04-21 15:13:38 -0300

    Add files via upload

---
+++ Fliper_SPOJ.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+  int p,r;
+
+  cin >> p >> r;
+
+
+  if( p == 0){
+    cout << "C" << endl;
+  }else if(p == 1){
+    if(r == 0){
+      cout << "B" << endl;
+    }else if(r == 1){
+      cout << "A" << endl;
+    }
+  }
+
+
+
+}
